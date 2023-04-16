2894764bac4d4d91a44f2c3cd9deb9882a44d3ed
zzqucppzz <phuquoc872003@gmail.com>
2023-04-16 15:14:58 +0700

    add backyard

--- Others/Backyard.java
+++ Others/Backyard.java
@@ -1,5 +1,8 @@
 package Others;
 
+import java.awt.event.MouseEvent;
+import java.lang.reflect.Array;
+
 public class Backyard {
     private int[] rows;
     private int[] columns;
@@ -12,23 +15,38 @@ public class Backyard {
     private void setRowsCoordinates(){
         rows = new int[5];
         for(int i = 0; i < 5; i++){
-            rows[i] = 20 + i*100;
+            rows[i] = 130 + i*100;
         }
     }
 
     private void setColumnsCoordinates(){
         columns = new int[10];
-        for(int i = 0; i < 9; i++){
-            rows[i] = 160 + i*80;
+        for(int i = 0; i < 10; i++){
+            columns[i] = 210 + i*80;
         }        
     }
 
-    public void addZombies(){
-
+    public int[] getRows(){
+        return rows;
     }
 
-    public void addPlants(){
+    public int[] getColumns(){
+        return columns;
+    }   
 
+    public int[] qualifiedPosition(MouseEvent e){
+        int[] a = {0,0,0};
+        for(int i = 0; i < 5;i++){
+            for(int j = 1;j < 10;j++){
+                if (((e.getX() - columns[j])*(e.getX() - columns[j])/(40*40) + (e.getY() - rows[i])*(e.getY() - rows[i])/(50*50)) <= 1){
+                    a[1] = rows[i];
+                    a[0] = columns[j];
+                    a[2] = 1;
+                    return a;
+                }  
+            }   
+        }
+        return a;
     }
 
 }
